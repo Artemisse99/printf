f020bc61de03a573222c3ca8a91418d4dca9df85
Artemisse99 <egonzalesp@unicesar.edu.co>
2021-11-08 13:49:23 -0500

    Print functions

---
+++ printfunctions.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdarg.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+/**
+ * _printf_c - print characters
+ * @arguments: gjhgkj
+ * @buffer: jhvjhjh
+ * @strmp: hikjhojh
+ */
+void _printf_c(va_list arguments, char *buffer, char *strtmp, char *tmp,
+		  int *j)
+{
+	arguments = arguments;
+	strmp = strmp;
+	tmp = tmp;
+
+	buffer[j] = (char)va_arg(arg, int);
+}
