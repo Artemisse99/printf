7dab279b42bd4916d1aa72a5cd758320bc16f8cd
Artemisse99 <egonzalesp@unicesar.edu.co>
2021-11-09 17:07:05 -0500

    main

--- main.h
+++ main.h
@@ -11,7 +11,7 @@ void _printf_c(va_list arg, char *buffer, char *strtmp, char *tmp, int *indj);
 char *_strcpy(char *dest, char *src);
 int _strlen(char *s);
 void _printf_s(va_list arg, char *buffer, char *strtmp, char *tmp, int *indj);
-void _printf_per(va_list arg, char *buffer, char *strtmp, char *tmp, int *indj);
+void _printf_p(va_list arg, char *buffer, char *strtmp, char *tmp, int *indj);
 void _printf_d(va_list arg, char *buffer, char *tmp, char *strtmp, int *indj);
 void _printf_i(va_list arg, char *buffer, char *tmp, char *strtmp, int *indj);
 char *_itoa(int i, char *strout, int base);
