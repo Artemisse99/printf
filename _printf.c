2f9922b52a6378c51a6a721abc5c0a31e761add4
Artemisse99 <egonzalesp@unicesar.edu.co>
2021-11-09 17:16:32 -0500

    printf

--- _printf.c
+++ _printf.c
@@ -9,39 +9,27 @@ int _printf(const char *format, ...)
 	va_list arg;
 	int i = 0, j = 0;
 	int *indi = &i, *indj = &j;
-	char buffer[4000], tmp[250];
-	char * strtmp = NULL;
+	char buffer[4000], tmp[250], *strtmp = NULL;
 	void (*selectf)(va_list, char *, char *, char *, int *);
 
 	if (format == NULL || arg == NULL || (format[0] == '%' && format[1] == '\0'))
-	{
 		return (-1);
-	}
-
 	va_start(arg, format);
-
-	while(format && format[*indi])
+	while (format && format[*indi])
 	{
-		if(format[*indi] != '%')
-		{
+		if (format[*indi] != '%')
 			buffer[*indj] = format[*indi];
-		}
 		else
 		{
 			(*indi)++;
-
 			selectf = get_functions((char *)format, *indi);
-
 			if (selectf == NULL)
 			{
 				(*indi)--;
 				buffer[*indj] = '%';
 			}
-
 			else
-			{
 				selectf(arg, buffer, tmp, strtmp, indj);
-			}
 		}
 		(*indi)++;
 		(*indj)++;
