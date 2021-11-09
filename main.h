360363380582517c0448e3ae88c90776d07692e1
pablito <3387@holbertonschool.com>
2021-11-09 11:08:18 -0500

    we added the prototypes of the new functions.

--- main.h
+++ main.h
@@ -3,13 +3,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdarg.h>
+
 int _printf(const char *format, ...);
 int _putchar(char c);
-void(*get_functions(char *format, int i))(va_list, char *, char *, char *, int*);
+void(*get_functions(char *format, int i))(va_list, char *, char *, char *,
+					  int*);
 void _printf_c(va_list arg, char *buffer, char *strtmp, char *tmp, int *indj);
 char *_strcpy(char *dest, char *src);
 int _strlen(char *s);
-void _printf_s(va_list arguments, char *buffer, char *strtmp, char *tmp,int *j);
+void _printf_s(va_list arg, char *buffer, char *strtmp, char *tmp,int *indj);
+void _printf_per(va_list arg, char *buffer, char *strtmp, char *tmp, int *indj);
+void _printf_d(va_list arg, char *buffer, char *tmp, char *strtmp, int *indj);
+void _printf_i(va_list arg, char *buffer, char *tmp, char *strtmp, int *indj);
+char *_itoa(int i, char *strout, int base);
+char *_strrev(char *string);
 
 typedef struct clasification
 {
