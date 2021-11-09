3ed875274545cfeab3c8f12c04316190b0a049a7
Artemisse99 <egonzalesp@unicesar.edu.co>
2021-11-09 16:58:56 -0500

    get functions

--- get_functions.c
+++ get_functions.c
@@ -4,12 +4,12 @@
 #include <string.h>
 #include <stdio.h>
 /**
- * get_op_cases - Function converts numbers to char.
+ * get_functions - Function converts numbers to char.
  * Return: Void.
  * @format: Sting being searched for %
  * @i: holds the place in which % was found in the string
  */
-void(*get_functions(char *format, int i))(va_list, char *, char *, char *, int*)
+void(*get_functions(char *format, int i))(va_list, char*, char*, char*, int*)
 {
 	int v = 0;
 	clf options[] = {
@@ -19,9 +19,9 @@ void(*get_functions(char *format, int i))(va_list, char *, char *, char *, int*)
 		{"d", _printf_d},
 		{"i", _printf_i},
 		{NULL, NULL}};
-	while(options[v].op != NULL)
+	while (options[v].op != NULL)
 	{
-		if(*options[v].op == format[i])
+		if (*options[v].op == format[i])
 		{
 			return (*options[v].funct);
 		}
