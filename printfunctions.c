03fbf75792f7a9b6681b9dc211b9274fa95e368c
pablito <3387@holbertonschool.com>
2021-11-08 19:17:49 -0500

    I added a function that allows covert the arguments to string.

--- printfunctions.c
+++ printfunctions.c
@@ -18,3 +18,34 @@ void _printf_c(va_list arg, char *buffer, char *strtmp, char *tmp,
 
 	buffer[*indj] = (char)va_arg(arg, int);
 }
+
+
+/**
+ * _printf_s - Function pointed to to assist in print a string
+ * Return: Void.
+ * @arg: Variadic arguements
+ * @buffer: Array in which what is to be printed is allocated
+ * @tmp: Array to hold values temporarily
+ * @strtmp: Pointer to hold values temporarily
+ * @j: Index that holds the position on buffer
+ */
+void _printf_s(va_list arg, char *buffer, char *strtmp, char *tmp,
+int *indj)
+{
+	tmp = tmp;
+
+	strtmp = va_arg(arg, char *);
+	if (strtmp == (char *)0)
+	{
+		strtmp = "(null)";
+	}
+	if (*strtmp == '\0')
+	{
+		(*indj)--;
+	}
+	else
+	{
+		_strcpy(&buffer[*indj], strtmp);
+		*indj = *indj + _strlen(strtmp);
+	}
+}
